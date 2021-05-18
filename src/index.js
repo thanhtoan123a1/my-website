f165dc2ff0b419e68a4f3df9417f023ef3ac230b
unknown <thanhtoan123a1@gmail.com>
2021-05-18 20:50:11 +0700

    fix: wrong open grap content

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // styles for this kit
-import "assets/css/bootstrap.min.css";
-import "assets/scss/now-ui-kit.scss?v=1.4.0";
-import "assets/demo/demo.css?v=1.4.0";
-import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+import 'assets/css/bootstrap.min.css';
+import 'assets/scss/now-ui-kit.scss?v=1.4.0';
+import 'assets/demo/demo.css?v=1.4.0';
+import 'assets/demo/nucleo-icons-page-styles.css?v=1.4.0';
 // pages for this kit
-import Index from "views/Index.js";
-import ProfilePage from "views/profile/ProfilePage.js";
-import LoginPage from "views/login/LoginPage.js";
+import Index from 'views/Index.js';
+import ProfilePage from 'views/profile/ProfilePage.js';
+import LoginPage from 'views/login/LoginPage.js';
 
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n/i18n.js";
-import Courses from "views/courses/index.js";
-import NotFound from "views/notFound/index.js";
-import Entertainment from "views/entertainment/index.js";
-import configureStore from "redux/store/index.js";
-import DarkFooter from "components/Footers/DarkFooter.js";
-import Header from "components/Navbars/Header.js";
-import Slug from "views/courses/slug.js";
-import { AuthProvider } from "components/contexts/AuthContext.js";
-import PrivateRoute from "components/PrivateRoute.js";
-import Home from "views/home/index.js";
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n.js';
+import Courses from 'views/courses/index.js';
+import NotFound from 'views/notFound/index.js';
+import Entertainment from 'views/entertainment/index.js';
+import configureStore from 'redux/store/index.js';
+import DarkFooter from 'components/Footers/DarkFooter.js';
+import Header from 'components/Navbars/Header.js';
+import Slug from 'views/courses/slug.js';
+import { AuthProvider } from 'components/contexts/AuthContext.js';
+import PrivateRoute from 'components/PrivateRoute.js';
+import Home from 'views/home/index.js';
+import Head from 'components/HeadTag/index.js';
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
     <Provider store={configureStore()}>
       <AuthProvider>
         <BrowserRouter>
+          <Head
+            title="Toan Tran Van Thanh"
+            description="Nơi học tập và chia sẻ"
+            ogUrl={window.location.href}
+            ogImageUrl={require('assets/img/favicon-page.png')}
+          />
           <Header />
           <Switch>
             <Switch>
@@ -72,5 +79,5 @@ ReactDOM.render(
       </AuthProvider>
     </Provider>
   </I18nextProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
